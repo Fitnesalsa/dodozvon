7e2b13179a57234ded424411cc73607f4cb9c31c
deniswj <denis.ultimate@gmail.com>
2022-04-23 11:04:42 +0200

    - добавлен запуск main.run()

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dodo_openapi import DodoOpenAPIParser, DodoOpenAPIStorer
 
 
-def main():
+def run():
     # update unit ids
     api_parser = DodoOpenAPIParser()
     api_storer = DodoOpenAPIStorer()
     api_result = api_parser.parse_unit_info()
     api_storer.store_unit_info(api_result)
 
-    
+if __name__ == '__main__':
+    run()
