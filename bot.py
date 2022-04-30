1a734e686046400ca91afb7e2be6dab45d2928d4
deniswj <denis.ultimate@gmail.com>
2022-04-30 18:35:28 +0200

    - minor fix

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ class Bot:
     def __init__(self):
         self._api_url = 'https://api.telegram.org/'
         self._token = config.TG_BOT_TOKEN
-        self._admin_id = config.TG_ADMIN_ID
+        self._admin_ids = config.TG_ADMIN_ID
 
     def _get_updates(self) -> None:
         url = f'{self._api_url}bot{self._token}/getUpdates'
@@ -15,6 +15,7 @@ class Bot:
     def send_message(self, message_text: str):
         self._get_updates()
         url = f'{self._api_url}bot{self._token}/sendMessage'
-        data = {'chat_id': self._admin_id,
-                'text': message_text}
-        requests.post(url, data=data)
+        for admin_id in self._admin_ids:
+            data = {'chat_id': admin_id,
+                    'text': message_text}
+            requests.post(url, data=data)
