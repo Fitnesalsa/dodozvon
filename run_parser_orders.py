3f555b48e4611f0062fbdd8a55feae41945f170e
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-06-30 17:56:16 +0300

    Add current time in output message and commented bot message feature.

--- run_parser_orders.py
+++ run_parser_orders.py
@@ -3,6 +3,7 @@ from parameters import ParametersGetter
 from postgresql import Database
 from dodo_openapi import DodoOpenAPIParser, DodoOpenAPIStorer
 from dodois import DodoAuthError, DodoEmptyExcelError, DodoResponseError
+#from bot import Bot
 
 from zipfile import BadZipFile
 
@@ -12,11 +13,13 @@ import time
 
 
 def main():
+    #bot = Bot()
     cur_time = datetime.now().strftime('%H:%M:%S')
     print(f'{cur_time}: Begin parsing process.')
+    #bot.send_message(f'{cur_time}: Begin parsing process.')
     db = Database()
     db.connect()
-
+    
     # Units table data update.
     api_parser = DodoOpenAPIParser()
     api_storer = DodoOpenAPIStorer(db=db)
@@ -30,19 +33,26 @@ def main():
     # Pass to orders parser.
     for (id_, *params_set) in params:
         try:
-            print(f'Parsing id {id}, params {params_set}...')
+            cur_time = datetime.now().strftime('%H:%M:%S')
+            print(f'{cur_time}: Parsing id {id}, params {params_set}...')
+            #bot.send_message(f'Parsing id {id}, params {params_set}...')
             dodois_parser_orders = DodoISParserOrders(*params_set)
             dodois_storer_orders = DodoISStorerOrders(id_, db=db)
             dodois_result_orders = dodois_parser_orders.parse()
             dodois_storer_orders.store(dodois_result_orders)
         except (ValueError, BadZipFile) as err:
-            print(f'Something went wrong with {params_set[1]}: {err}')
+            cur_time = datetime.now().strftime('%H:%M:%S')
+            print(f'{cur_time}: Something went wrong with {params_set[1]}: {err}')
+            #bot.send_message(f'{cur_time}: Something went wrong with {params_set[1]}: {err}')
         except (DodoAuthError, DodoResponseError, DodoEmptyExcelError) as e:
-            print(f'{params_set[1]}: {e.message}')
+            cur_time = datetime.now().strftime('%H:%M:%S')
+            print(f'{cur_time}: {params_set[1]}: {e.message}')
+            #bot.send_message(f'{cur_time}: {params_set[1]}: {e.message}')
     
     db.close()
     cur_time = datetime.now().strftime('%H:%M:%S')
     print(f'{cur_time}: Parsing process succesfully end.\nWaiting...')
+    #bot.send_message(f'{cur_time}: Parsing process succesfully end.\nWaiting...')
 
 if __name__=='__main__':
     schedule.every(6).hours.do(main)
