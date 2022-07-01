e2ca89221fd66fd0e1c8a2a39578991515cccb57
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-01 18:26:36 +0300

    Add initialization method

--- tasker_orders.py
+++ tasker_orders.py
@@ -5,10 +5,18 @@ import config
 from storage import YandexDisk
 from parser import DatabaseWorker
 from postgresql import Database
+from datetime import datetime
 
 
 class DatabaseTaskerOrders(DatabaseWorker):
-    def __init__(self, db: Database=None):
+    
+    
+    def __init__(self, db: Database=None, begin_date: datetime,
+            end_date: datetime, upload_all: bool=True):
         self._storage = YandexDisk()
+        self._begin_date = begin_date
+        self._end_date = end_date
+        self._upload_all = upload_all
+        self._pizzerias = []
         super().__init__(db)
 
