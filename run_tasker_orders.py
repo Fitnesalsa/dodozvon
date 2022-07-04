5416371b130a040b977aa7d10c35db124dedd180
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-04 15:13:57 +0300

    Add TODO comments for exceptions handling.

--- run_tasker_orders.py
+++ run_tasker_orders.py
@@ -7,12 +7,22 @@ from storage import YandexCreateFolderError, YandexUploadError, YandexFileNotFou
 def main():
     #bot = Bot()
     
+    # TODO: Add exceptions handling:
+    # TODO: Wrong date format; begin_date > end_date.
+    # TODO: Nonexisting file; wrong fileformat.
+    # TODO: Wrong answer.
     print('>> Please, enter date range (dd.mm.yyyy):') 
     begin_date = input('>> Begin date: ')
     end_date = input('>> End date: ')
+    upload_all = input('>> Upload data for all available pizzerias(y/n):\n>> ')
+    if upload_all.lower() == 'y':
+        upload_all = True
+    else:
+        upload_all = False
 
     try:
         db_tasker_orders = DatabaseTaskerOrders(
+                upload_all=upload_all,
                 end_date=end_date,
                 begin_date=begin_date
                 )
