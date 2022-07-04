44cfeb34253f6009d973dde1201b33ecf2c26280
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-04 12:56:23 +0300

    Add running file for tasker orders

---
+++ run_tasker_orders.py
@@ -0,0 +1,12 @@
+from tasker_orders import DatabaseTaskerOrders
+from bot import Bot
+from config import YANDEX_ORDERS_FOLDER
+from storage import YandexCreateFolderError, YandexUploadError, YandexFileNotFound
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
