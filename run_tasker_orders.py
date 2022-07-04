5727f839f2cfdf590d9576ba56809085f09dfc90
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-07-04 13:08:12 +0300

    Add main execution flow logic.

--- run_tasker_orders.py
+++ run_tasker_orders.py
@@ -5,7 +5,26 @@ from storage import YandexCreateFolderError, YandexUploadError, YandexFileNotFou
 
 
 def main():
-    pass
+    #bot = Bot()
+    
+    print('>> Please, enter date range (dd.mm.yyyy):') 
+    begin_date = input('>> Begin date: ')
+    end_date = input('>> End date: ')
+
+    try:
+        db_tasker_orders = DatabaseTaskerOrders(
+                end_date=end_date,
+                begin_date=begin_date
+                )
+        db_tasker_orders.upload()
+        db_tasker_orders.db_close()
+    except (YandexCreateFolderError,
+            YandexUploadError,
+            YandexFileNotFound) as e:
+        print('>> ' + e.message)
+    
+    print('>> Upload is successfully completed.')
+
 
 if __name__ == '__main__':
     main()
